c7fa3683db39b7b0b17f73ba3b40cdce9582b262
Simon Lanza Fiorito <simonlanza99@gmail.com>
2023-11-02 15:51:54 -0300

    adjust

--- FrontEnd/master-events/src/components/InputField/InputField.jsx
+++ FrontEnd/master-events/src/components/InputField/InputField.jsx
@@ -7,7 +7,7 @@ const InputField = ({ type, placeholder, value, onChange }) => {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className="w-full py-3 px-4 my-2 rounded-lg bg-zinc-200 focus:outline-none"
+      className="w-full py-3 px-4 my-1 rounded-lg bg-zinc-200 focus:outline-none"
       style={{ borderRadius: '25px', boxShadow: 'inset 2px 2px 5px #babecc, inset -5px -5px 10px #ffffff73' }}
     />
   );
