22c7a334afb5a4594c8bbe54720f7a09e6921e0d
Hector Javier Moreno <cytelsystem@gmail.com>
2023-11-02 16:06:56 -0500

    se adiconanron eventos header para cambiar la posicion del footer al hacer click en los botones

--- FrontEnd/master-events/src/components/Header/Header.js
+++ FrontEnd/master-events/src/components/Header/Header.js
@@ -3,23 +3,45 @@ import './Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const handleFooterFix = () => {
+    const footerElement = document.querySelector('.footer');
+    if (footerElement) {
+      footerElement.style.position = 'fixed';
+      footerElement.style.bottom = '0';
+    }
+  };
+
+  const handleRemoveFooterFix = () => {
+    const footerElement = document.querySelector('.footer');
+    if (footerElement) {
+      footerElement.style.position = ''; // Restablecer la posición predeterminada
+      footerElement.style.bottom = '';
+    }
+  };
+
   return (
     <header className="header">
       <div className="logo-container">
-        <Link to='/'>
-        <img src="/logo.svg" alt="MasterEvents Logo" width="100" height="57" />
+        <Link to='/' onClick={handleFooterFix }>
+          <img src="/logo.svg" alt="MasterEvents Logo" width="100" height="57" />
         </Link>
         <div className="header-text">
           MasterEvents
         </div>
       </div>
       <div className="button-container">
-        <button className="header-button">Crear Evento</button>
-        <Link to="/registration">
-          <button className="header-button">Crear cuenta</button>
+        <button className="header-button" onClick={handleFooterFix}>
+          Crear Evento
+        </button>
+        <Link to="/registration" onClick={handleFooterFix}>
+          <button className="header-button">
+            Crear cuenta
+          </button>
         </Link>
-        <Link to="/signin">
-          <button className="header-button">Iniciar sesión</button>
+        <Link to="/signin" onClick={handleFooterFix}>
+          <button className="header-button">
+            Iniciar sesión
+          </button>
         </Link>
       </div>
     </header>
